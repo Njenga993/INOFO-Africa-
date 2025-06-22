4b49d95fd405d487593b88ffb8a90abbf6963dd8
Njengak993 <njengak993@gmail.com>
2025-06-22 08:53:46 +0300

    preparing for githubpag

--- vite.config.ts
+++ vite.config.ts
@@ -3,5 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/INOFO-Africa-/', // 👈 Use your exact repo name here
   plugins: [react()],
 })
