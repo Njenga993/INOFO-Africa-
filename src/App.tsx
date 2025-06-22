6d0d762aa1803ee7d3c093e0f17fcc6dc298f2df
Njengak993 <njengak993@gmail.com>
2025-06-22 09:35:05 +0300

    preparing for githubpaes

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,8 @@
 // src/App.tsx
 import { useState, useEffect } from 'react';
 import AfricaLoader from '../components/AfricaLoader';
-import Navbar from '../components/Navbar'
-import Footer from '../components/Footer'
+import Navbar from '../components/Navbar';
+import Footer from '../components/Footer';
 import MembershipForm from '../pages/MembershipForm';
 import { Routes, Route } from 'react-router-dom'
 import Home from '../pages/Home';
