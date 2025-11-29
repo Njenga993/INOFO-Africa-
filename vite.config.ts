b198a95ff76535cc3c8e7250c2ac132b1f649556
Njengak993 <njengak993@gmail.com>
2025-11-29 15:07:41 +0300

    changed the a few important

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,6 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  base: '/',  // 👈 include the dash at the end
+  base: '/INOFO-Africa-/',  // 👈 include the dash at the end
   plugins: [react()],
 })
