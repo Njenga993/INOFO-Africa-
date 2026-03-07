23626edb5031426f2eff51d7e61bd6f6ce3c0b09
Njengak993 <njengak993@gmail.com>
2026-03-07 06:45:06 +0300

    added the component

--- src/main.tsx
+++ src/main.tsx
@@ -9,7 +9,7 @@ import './index.css';
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <HelmetProvider>   {/* 👈 Wrap the entire app */}
-      <BrowserRouter basename="/INOFO-Africa-/">
+      <BrowserRouter basename="/">
         <App />
       </BrowserRouter>
     </HelmetProvider>
