d92a8a3d9a4c611d31c51ac94e037872ce513acb
Njengak993 <njengak993@gmail.com>
2026-04-02 15:15:46 +0300

    added news to the resources page

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,6 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig({
-  base: "/", // 👈 include the dash at the end
+  base: "/INOFO-Africa-/", // 👈 include the dash at the end
   plugins: [react()],
 });
